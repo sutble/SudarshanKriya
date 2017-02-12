66b7f73c219f1ecfc78261b86f8e1d50fb2f3b56
Aditya Garg <sutble@bitbucket.org>
2017-02-11 22:58:07 -0800

    Working model before big refactor change

---
+++ SnapchatCamera/SampleScene.swift
@@ -0,0 +1,13 @@
+//
+//  SampleScene.swift
+//  SudarshanKriya
+//
+//  Created by Aditya Garg on 2/9/17.
+//  Copyright © 2017 Archetapp. All rights reserved.
+//
+
+import UIKit
+
+class SampleScene: SKScene {
+
+}
